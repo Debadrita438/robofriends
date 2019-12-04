481bc22cd00e56498e1059878fb5d8710b5af832
Debadrita438 <bristibandyopadhyay@gmail.com>
2019-12-04 20:41:31 +0530

    Updates

---
+++ precache-manifest.e2a7b88fdbf51b94e9f202b77f89935d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e496099a74f768f5a0b8b4123e3f005e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f2f1c8e3f133e5e7b2a9",
+    "url": "/robofriends/static/css/main.b1638452.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "f2f1c8e3f133e5e7b2a9",
+    "url": "/robofriends/static/js/main.58fd1a7f.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
